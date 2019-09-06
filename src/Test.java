382c234a97eca42aebad1d0c2586fe8c6ec0d1b6
Tambi <703248700@genpact.com>
2019-09-06 16:14:02 +0530

    new git

---
+++ src/Test.java
@@ -0,0 +1,29 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		//assignment 1
+		// double total_cost;
+		// int nItems;
+		// float priceUnit;
+		
+		// total_cost = nItems + priceUnit
+				
+		//assignment 2
+		int a = 2;
+		int b = 3;
+		int c = 4;
+		int d = 5;
+		int e = 7;
+		
+		float f = (a+b+c+d+e)/5.0f;
+		System.out.println(f);
+
+		
+		// System.out.println("Hello");
+		// TODO Auto-generated method stub
+			
+
+	}
+
+}
